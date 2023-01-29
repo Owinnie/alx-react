f541afbf74b4e78eab7ffacab61bae851a437825
Owinnie <tillawinnie@gmail.com>
2023-01-29 22:04:42 +0300

    basic components

---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+  )
+}
+
+export default Footer
