d057d8379dd1ee13089ecbc496f7a23059939e5d
Owinnie <tillawinnie@gmail.com>
2023-02-02 22:54:46 +0300

    one requirement error

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -9,7 +9,11 @@ import { getLatestNotification } from '../utils/utils';
 import PropTypes from 'prop-types';
 
 
-class App extends Component {  
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  }
 
   render() {
     const listCourses = [
