538b64f129730f8469889b83a8118d3ec685c4e5
Owinnie <tillawinnie@gmail.com>
2023-02-12 20:27:45 +0300

    1. controlled comp

--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 function Login() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [enableSubmit, setenableSubmit] = useState(false)
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault()
+    setIsLoggedIn(true)
+  }
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  useEffect(() => {
+    if (email != '' && password != '') {
+      setEnableSubmit(true);
+    } else {
+      if (enableSubmit != false) {
+        setEnableSubmit(false);
+      }
+    }
+  }, [email, password]);
+
   return (
     <React.Fragment>
       <div className={css(body.App)}>
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" name="email" className={css(body.input)} />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" name="password" className={css(body.input)} />
-        <button>OK</button>
+        <form onSubmit={handleLoginSubmit}>
+          <label htmlFor="email">Email: </label>
+          <input type="email"
+            id="email" name="email"
+            className={css(body.input)}
+            value={email} onChange={handleChangeEmail} />
+          <label htmlFor="password">Password: </label>
+          <input type="password"
+            id="password" name="password"
+            className={css(body.input)}
+            value={password} onChange={handleChangePassword} />
+          <input type="submit" value="Ok" disabled={!enableSubmit} />
+        </form>
       </div>
     </React.Fragment>
   )
